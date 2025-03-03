e179564e996de38340016b27ce6a65216953f449
asmae <asmaeaouassar333@example.com>
2025-03-03 02:34:43 +0100

    ajouter une fonctionnalité qui permet de voir l'évaluation attribut à chaque rapport

--- src/app/professeur/rapports-stage/rapports-stage.component.ts
+++ src/app/professeur/rapports-stage/rapports-stage.component.ts
@@ -1,7 +1,7 @@
 import { DatePipe, NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component ,ElementRef,inject,OnInit,ViewChild} from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { DepotRapportStage } from '../../model/model';
+import { DepotRapportStage, Evaluation } from '../../model/model';
 import { ServiceService } from '../../service/service.service';
 import { StatutRapport } from '../../model/enums';
 import { stat } from 'fs';
@@ -12,10 +12,17 @@ import { stat } from 'fs';
   templateUrl: './rapports-stage.component.html',
   styleUrl: './rapports-stage.component.css'
 })
-export class RapportsStageComponent {
+export class RapportsStageComponent implements OnInit{
+
   listDepotsRapport:DepotRapportStage[]=[];
 
-  constructor(private service:ServiceService){
+  // Modal pour l'évaluation d'un rapport
+  @ViewChild('evaluationmodal') evaluationmodal: ElementRef | undefined;
+
+  evaluationDepotSelectionne!:Evaluation | null | undefined;
+
+  service=inject(ServiceService);
+  ngOnInit(){
     this.listDepotsRapport=this.service.getRapports();
   }
 
@@ -26,6 +33,22 @@ export class RapportsStageComponent {
         return 'rgb(68, 193, 68)';
       case StatutRapport.NV:
         return 'rgb(255, 70, 70)';
+      default:
+        return 'grey';
+    }
+  }
+
+  voirEvaluationOpenModal(idDepotRapport:number){
+    if(this.evaluationmodal){
+      this.evaluationmodal.nativeElement.style.display='block';
+      const depotStageCourant=this.service.getDepotById(idDepotRapport);
+      this.evaluationDepotSelectionne=depotStageCourant?.evaluation;
+    }
+  }
+
+  closeModal(){
+    if(this.evaluationmodal){
+      this.evaluationmodal.nativeElement.style.display='none';
     }
   }
 }
