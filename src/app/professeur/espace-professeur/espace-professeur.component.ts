52e4ddb699bf52c19dc4f7f824cb9a1fe694735a
asmae <asmaeaouassar333@example.com>
2025-03-16 14:17:24 +0100

    mettre la barre de recherche des étudiants fonctionnelle

--- src/app/professeur/espace-professeur/espace-professeur.component.ts
+++ src/app/professeur/espace-professeur/espace-professeur.component.ts
@@ -2,17 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Student } from '../../model/model';
 import { ServiceService } from '../../service/service.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-espace-professeur',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './espace-professeur.component.html',
   styleUrl: './espace-professeur.component.css'
 })
 export class EspaceProfesseurComponent {
 
   isDropdownOpen = false;
+
+  searchStudent : string ='';
+
+  filtreEtudiants:any[]=[]; //1
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
@@ -22,5 +27,21 @@ export class EspaceProfesseurComponent {
   listEtudiantsEncadre:Student[]=[];
   constructor(private service:ServiceService){
     this.listEtudiantsEncadre=this.service.getStudents();
+    this.filtreEtudiants=[...this.listEtudiantsEncadre]; //2
   }
+
+
+
+// 3 
+  filtrerEtudiants(){
+    this.filtreEtudiants=this.listEtudiantsEncadre.filter(
+      etudiant=> (etudiant.prenom+' '+etudiant.nom).toLowerCase().includes(this.searchStudent.toLowerCase())
+    );
+
+    for(let e of this.filtreEtudiants){
+      console.log(e.nom+' '+e.prenom);
+    }
+  }
+
+
 }
